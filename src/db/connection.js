3e6b89b34f7345d561e54d95842bceda52f0c5c2
juanGomez <gomezjan@yahoo.com.ar>
2024-06-06 13:20:32 -0300

    8 commit

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,7 +1,8 @@
-
+import mysql from 'mysql';
+import keys from '../keys';
 "use strict";
 exports.__esModule = true;
 var mysql_1 = require("mysql");
 var keys_1 = require("../keys");
-var connection = mysql_1["default"].createConnection(keys_1["default"]);
+const connection = mysql.createConnection(keys);
 exports["default"] = connection;
